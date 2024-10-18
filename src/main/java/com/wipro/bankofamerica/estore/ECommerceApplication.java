c7e86f63642dade265d5e97983c7bc3249bf9246
shubhangik25 <khalkarshubhangi1595@gmail.com>
2024-10-18 10:58:14 +0530

    commited by shubhangi

--- src/main/java/com/wipro/bankofamerica/estore/ECommerceApplication.java
+++ src/main/java/com/wipro/bankofamerica/estore/ECommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerce1Application {
+public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerce1Application.class, args);
+		SpringApplication.run(ECommerceApplication.class, args);
 	}
 
 }
